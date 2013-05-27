ab94526439b33dd29467018747c275167528ef95
KirilKostov <kiril_lol@abv.bg>
2013-05-28 01:47:15 +0300

    Commiting initial collection.rb file

--- collection.rb
+++ collection.rb
@@ -1,2 +1,9 @@
 class Collection
   include Enumerable
+
+    def initialize(songs)
+      @names   = songs.map { |song| song.name   }.uniq
+      @artists = songs.map { |song| song.artist }.uniq
+      @albums  = songs.map { |song| song.album  }.uniq
+      @songs   = songs
+    end
