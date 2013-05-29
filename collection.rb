3ec7f11d6aeb9efe9ac67423b4480f350bca592d
KirilKostov <kiril_lol@abv.bg>
2013-05-29 16:17:22 +0300

    Add artists, albums, names methods

--- collection.rb
+++ collection.rb
@@ -1,11 +1,41 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'song'
+
 class Collection
   include Enumerable
 
-  attr_reader :songs, :names, :artists, :albums
+  attr_reader :songs
 
   def initialize(songs)
-    @names   = songs.map { |song| song.name   }.uniq
-    @artists = songs.map { |song| song.artist }.uniq
-    @albums  = songs.map { |song| song.album  }.uniq
-    @songs   = songs
+    @songs = songs
   end
+
+  def artists
+    songs.collect { |song| song.artist }.uniq
+  end
+
+  def albums
+    songs.collect { |song| song.album }.uniq
+  end
+
+  def names
+    songs.collect { |song| song.name }.uniq
+  end
+
+  def self.parse(text)
+    file = File.open(text)
+    contents = file.read
+    file.close
+    # --------------------------
+    lines = contents.split("\n").reject { |str| str == '' }
+    
+    songs = lines.each_slice(3).map do |slice|
+      Song.new(slice[0], slice[1], slice[2])
+    end
+
+    Collection.new(songs)
+  #puts contents #implement parsing of the data
+  end
+
+end
